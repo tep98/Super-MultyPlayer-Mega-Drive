62a80771c7d676a7bbde57acdb69156618d9d263
Zaklzzzy <zaletin123@gmail.com>
2024-04-02 13:03:33 +0300

    fix: fix UI (need changes)

--- Assets/Scripts/Damageble.cs
+++ Assets/Scripts/Damageble.cs
@@ -2,42 +2,122 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using Mirror;
+using Mirror.Examples.Basic;
 
 public class Damageble : NetworkBehaviour
 {
-    [SerializeField] private float _health;
-    [SerializeField] private Text text;
-    [SerializeField] private GameObject losePanel;
-    [SerializeField] private Animator anim;
-    [SerializeField] private GameObject _canvas;
+    public event System.Action<float> OnPlayerHealthChanged;
+    public event System.Action<bool> OnPlayerLoseChanged;
+    public event System.Action<string> OnPlayerAnimChanged;
 
-    private void Start()
+    [Header("Health")]
+    [SerializeField] private float _health;
+
+    [Header("Player UI")]
+    [SerializeField] private GameObject playerUIPrefab;
+    GameObject playerUIObject;
+    PlayerUI playerUI = null;
+
+    #region SyncVars
+    [Header("SyncVars")]
+    //Hooks для вызова методов изменений
+    [SyncVar(hook = nameof(PlayerHealthChanged))]
+    public float playerHealth = 50;
+
+    [SyncVar(hook = nameof(PlayerLoseChanged))]
+    public bool status = false;
+
+    [SyncVar(hook = nameof(PlayerAnimChanged))]
+    public string triggerName = "Damage";
+
+    //Методы для вызова в Hook, которые находятся выше
+    void PlayerHealthChanged(float _, float newPlayerHealth)
+    {
+        OnPlayerHealthChanged?.Invoke(newPlayerHealth);
+    }
+    void PlayerLoseChanged(bool _, bool newPlayerLose)
+    {
+        OnPlayerLoseChanged?.Invoke(newPlayerLose);
+    }
+    void PlayerAnimChanged(string _, string newPlayerAnim)
+    {
+        OnPlayerAnimChanged?.Invoke(newPlayerAnim);
+    }
+    #endregion
+
+
+    #region Client
+
+    public override void OnStartClient()
+    {
+        // Instantiate the player UI as child of the Players Panel
+        playerUIObject = Instantiate(playerUIPrefab, CanvasUI.GetPlayersPanel());
+        playerUIObject.SetActive(true);
+        playerUI = playerUIObject.GetComponent<PlayerUI>();
+
+        playerHealth = 50;
+        syncDirection = SyncDirection.ClientToServer;
+
+        // wire up all events to handlers in PlayerUI
+        OnPlayerHealthChanged = playerUI.HealthTextChange;
+        OnPlayerLoseChanged = playerUI.SetActiveLose;
+        OnPlayerAnimChanged = playerUI.AnimSetTrigger;
+
+        // Invoke all event handlers with the initial data from spawn payload
+        OnPlayerHealthChanged.Invoke(playerHealth);
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        // Activate the main panel
+        CanvasUI.SetActive(true);
+        playerUIObject.SetActive(false);
+    }
+
+
+    private void UIChanger()
     {
         if (!isLocalPlayer)
         {
-            _canvas.SetActive(false);
-
+            playerUIObject.SetActive(false);
         }
         else
         {
-            _canvas.SetActive(true);
-            text.text = _health.ToString() + "HP";
+            playerUIObject.SetActive(true);
         }
     }
 
     public void TakeDamage(float damage)
     {
         _health -= damage;
-        anim.SetTrigger("Damage");
-        text.text = _health.ToString() + "HP";
+        playerHealth -= damage;
+        OnPlayerAnimChanged.Invoke("Damage");
+        OnPlayerHealthChanged.Invoke(playerHealth);
 
         if (_health <= 0)
         {
-            text.text = 0 + "HP";
-            losePanel.SetActive(true);
+            OnPlayerHealthChanged.Invoke(0);
+            OnPlayerLoseChanged.Invoke(true);
             Invoke("RpcReloadScene", 1f);
         }
     }
+
+/*    public override void OnStopLocalPlayer()
+    {
+        // Disable the main panel for local player
+        CanvasUI.SetActive(false);
+    }*/
+/*    public override void OnStopClient()
+    {
+        // disconnect event handlers
+        OnPlayerHealthChanged = null;
+        OnPlayerLoseChanged = null;
+        OnPlayerAnimChanged = null;
+
+        // Remove this player's UI object
+        Destroy(playerUIObject);
+    }*/
+    #endregion
 
     [ClientRpc]
     private void RpcReloadScene()
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+
+public class PlayerUI : NetworkBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private Text text;
+    [SerializeField] private GameObject losePanel;
+    [SerializeField] private Animator anim;
+
+    public void AnimSetTrigger(string triggerName)
+    {
+        anim.SetTrigger(triggerName);
+    }
+
+    public void HealthTextChange(float health)
+    {
+        text.text = health.ToString() + " HP";
+    }
+
+    public void SetActiveLose(bool status)
+    {
+        losePanel.SetActive(status);
+    }
+}
